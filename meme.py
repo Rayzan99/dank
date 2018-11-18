5540f1c612e83535f9be83a021d2c0c60f72234d
Rayzan99 <43727454+Rayzan99@users.noreply.github.com>
2018-11-18 14:59:22 +0545

    Update meme.py

--- meme.py
+++ meme.py
@@ -65,7 +65,7 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
-    await client.change_status(game=discord.Game(name='Your Mom', type=1))
+    await client.change_status(game=discord.Game(name='w/ Your Mom', type=1))
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
